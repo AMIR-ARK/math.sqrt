abbc5eb804eeafa12429ce579ed171ddb610770e
AMIR-ARK <126375003+AMIR-ARK@users.noreply.github.com>
2023-06-25 08:20:45 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter value for x: ");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter value for y: ");
+        double y = double.Parse(Console.ReadLine());
+
+        double result = CalculateFunction(x, y);
+        Console.WriteLine("The result of the function is: {0}", result);
+
+        Console.ReadKey();
+    }
+
+    static double CalculateFunction(double x, double y)
+    {
+        double temp = Math.Abs(x) + Math.Sqrt(x);
+
+        return Math.Sqrt(temp) + y;
+    }
+}
